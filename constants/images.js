22bb2cb2c0050ddb04ff06bc8d7f3773a4576de1
Anoop TD <tdanoop19@gmail.com>
2020-09-14 14:12:06 +0530

    Images added

---
+++ constants/images.js
@@ -0,0 +1,8 @@
+export default {
+    category:{
+        '1':require(`../assets/app-images/seafood.png`),
+        '2':require(`../assets/app-images/backwater.png`),
+        '3':require(`../assets/app-images/dried.png`),
+        '4':require(`../assets/app-images/combo.png`),
+    }
+}
